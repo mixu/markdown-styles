ee4ad82e35367241ccb4611d8b28132dac0bbe7f
Mikito Takada <mikito.takada@gmail.com>
2015-01-22 22:08:19 -0800

    Add library files

--- lib2/headings-to-toc.js
+++
@@ -1,4 +0,0 @@
-'<ul class="nav nav-list">' +
-      item.headings.reduce(function(prev, item) {
-        return prev + '<li><a href="#'+item.id+'">'+item.title.replace(':', '')+'</a></li>';
-      }, '') + '</ul>'
