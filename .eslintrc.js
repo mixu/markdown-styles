c1e5661f62c3ae03fbb38c942bb35e9e0a2eb9ff
Mikito <mikito.takada@gmail.com>
2022-03-24 23:09:06 -0700

    WIP

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "ignorePatterns": ["layouts/*", "output/*", ''],
+    "rules": {
+    }
+};
